ae5257738571c8a6f7551c7fb8b8b1453a56c851
Hamza Shahid <P14164824@GWHXX680LIN008W.lec-admin.dmu.ac.uk>
2016-02-03 12:02:14 +0000

    added new method to math.c

--- math.c
+++ math.c
@@ -18,11 +18,16 @@ int remainder(int x, int y){
 	return x%y;
 }
 
+int addition2(int x, int y){
+	return x=y;
+}
+
 int main() {
 	int x=2, y=3;
 	printf("%i\n\n", addition(x,y));
 	printf("%i\n\n", subtraction(x,y));
 	printf("%i\n\n", multiplication(x,y));
 	printf("%f\n", division(x,y));
+	printf("%f\n", addition2(x,y));
 	return 0;
 }

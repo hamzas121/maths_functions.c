594b7f1e59b063413f2adf0d1d7ded6c29087a59
Hamza Shahid <P14164824@GWHXX582LIN019W.lec-admin.dmu.ac.uk>
2016-02-10 12:30:20 +0000

    added max and min

--- math.c
+++ math.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int addition(int x, int y) {
+	
 	return x+y;
 }
 int subtraction(int x, int y) {
@@ -18,8 +19,38 @@ int remainder(int x, int y){
 	return x%y;
 }
 
-int addition2(int x, int y){
-	return x=y;
+int addition2(int x, int y) {
+	printf("Write 5 numbers\n");
+
+    for (c = 1; c <= n; c++)
+    {
+        scanf_s("%d", &value);
+
+
+        if (c = 1)
+        {
+            max = value;
+            min = value;
+
+
+        }
+        else if (value < max, c <= n)
+        {
+            max = value;
+        }
+
+        else if (value < min, c <= n)
+        {
+            min = value;
+        }
+    }
+
+    printf("Biggest number is : %d\n", max);
+    printf("Smallest number is : %d\n", min);
+
+
+
+    
 }
 
 int main() {
